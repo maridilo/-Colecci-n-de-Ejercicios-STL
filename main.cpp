246ad5486d36a527158685900d09dd3946bf256d
maridilo <146822054+maridilo@users.noreply.github.com>
2023-11-15 17:39:27 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
 
+using namespace std;
+
+// Definición del espacio de nombres Matematicas
+namespace Matematicas {
+    //Función para sumar dos numeros
+    int suma(int a, int b) {
+        return a + b;
+    }
+    //Función para restar dos numeros
+    int resta(int a, int b) {
+        return a - b;
+    }
+}
+int main(){
+    cout <<"Suma: " << Matematicas::suma(5,7) << endl;
+    cout <<"Resta: " << Matematicas::resta(5,7) << endl;
+    return 0;
+}
